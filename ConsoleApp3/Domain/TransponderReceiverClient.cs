db8cf7b6f365faf4019c4b95ea4d60a13d38ebfc
BadaBoomski <20119144@post.au.dk>
2018-11-08 12:13:42 +0100

    added the TransponderReceiverClient from FRABJ + .dll

---
+++ ConsoleApp3/Domain/TransponderReceiverClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransponderReceiver;
+
+namespace ConsoleApp3.Domain
+{
+    public class TransponderReceiverClient
+    {
+        private ITransponderReceiver receiver;
+
+        public TransponderReceiverClient(ITransponderReceiver receiver)
+        {
+            this.receiver = receiver;
+
+            this.receiver.TransponderDataReady += ReceiverOnTransponderDataReady;
+        }
+
+        public void ReceiverOnTransponderDataReady(object sender, RawTransponderDataEventArgs e)
+        {
+            foreach (var data in e.TransponderData)
+            {
+                System.Console.WriteLine($"Transponderdata {data}");
+            }
+        }
+    }
+}
