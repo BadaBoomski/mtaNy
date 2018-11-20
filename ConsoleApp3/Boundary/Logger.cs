311364b05cf0f710b5356b830b5825e8d8030820
DuffyBaerbar <au564095>
2018-11-20 11:42:26 +0100

    added ILogger, Logger and a test for TranspoderReceiver

---
+++ ConsoleApp3/Boundary/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp3.Interfaces;
+using System.IO;
+
+namespace ConsoleApp3.Boundary
+{
+    public class Logger : ILogger
+    {
+
+        private readonly string _filePath;
+
+        public Logger(string filePath = null)
+        {
+            _filePath = filePath ?? "Log.txt";
+        }
+        public void Log(string tag, string date)
+        {
+            using (StreamWriter file = new StreamWriter(_filePath, true))
+                file.WriteLine(tag + date);
+
+
+        }
+    }
+}
---
+++ UnitTestProject1/TestLogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp3;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ConsoleApp3.Boundary;
+using ConsoleApp3.Interfaces;
+
+/*namespace UnitTestProject1
+{
+    [TestFixture()]
+    class TestLogger
+    {
+
+    }
+}
+*/
