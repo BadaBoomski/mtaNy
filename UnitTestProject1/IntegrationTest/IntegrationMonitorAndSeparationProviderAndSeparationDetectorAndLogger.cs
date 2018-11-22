121a8752d4402cdc200a895eb8aab9b42acd7eef
DuffyBaerbar <au564095>
2018-11-22 16:55:53 +0100

    added integrationstest for logger,detector,provider og monitor

---
+++ UnitTestProject1/IntegrationTest/IntegrationMonitorAndSeparationProviderAndSeparationDetectorAndLogger.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp3;
+using ConsoleApp3.Interfaces;
+using NUnit.Framework;
+using ConsoleApp3.Domain;
+using NSubstitute;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    class IntegrationMonitorAndSeparationProviderAndSeparationDetectorAndLogger
+    {
+        //Stubs
+        private IMonitor _monitor;
+        private ISeperationProvider _seperationProvider;
+        private ISeparationDetector _separationDetector;
+        private ITracksUpdated _tracksUpdated;
+        private ILogger _Logger;
+
+        //Unit under test
+        private SeparationDetector _uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            //Stubs
+            _monitor = Substitute.For<IMonitor>();
+            _separationDetector = Substitute.For<ISeparationDetector>();
+            _tracksUpdated = Substitute.For<ITracksUpdated>();
+            _Logger = Substitute.For<ILogger>();
+            //Unit under test
+            _uut = new SeparationDetector(_tracksUpdated, _Logger);
+        }
+
+        [Test]
+        public void IntegrationTester()
+        {
+            //create data
+            List<ITrack> trackList = new List<ITrack>();
+            string fileText;
+
+            Track fly1 = new Track
+            {
+                Timestamp = new DateTime(1995, 1, 1, 1, 1, 1),
+                Tag = "fly1",
+                Altitude = 700,
+                YCoordinate = 11000,
+                XCoordinate = 11000,
+                Course = 10,
+                Velocity = 150
+            };
+
+            Track fly2 = new Track
+            {
+                Timestamp = new DateTime(1995, 1, 1, 1, 1, 1),
+                Tag = "fly2",
+                Altitude = 700,
+                YCoordinate = 11000,
+                XCoordinate = 11000,
+                Course = 10,
+                Velocity = 150
+            };
+
+            trackList.Add(fly1);
+            trackList.Add(fly2);
+            var args = new TrackEvents(trackList);
+
+            //Create event
+            _tracksUpdated.TrackUpdated += Raise.EventWith(args);
+
+
+            //Assert
+            _monitor.Received(1).Clear();
+            _monitor.Received(1).Write("---- Planes Are To Damn Close -----");
+            _monitor.Received(1).Write(fly1.Tag + " And " + fly2.Tag + " " + new DateTime(1995, 1, 1, 1, 1, 1));
+            _monitor.Received(1).Write(fly2.Tag + " And " + fly1.Tag + " " + new DateTime(1995, 1, 1, 1, 1, 1));
+
+            using (StreamReader file = new StreamReader("log.txt"))
+            {
+                fileText = file.ReadLine();
+            }
+
+            Assert.That(fileText, Is.EqualTo(fly1.Tag + " Has started a seperation event with " + fly2.Tag + " ; " + fly1.Timestamp.ToString()));
+            Assert.That(fileText, Is.EqualTo(fly2.Tag + " Has started a seperation event with " + fly1.Tag + " ; " + fly2.Timestamp.ToString()));
+
+        }
+    }
+}
