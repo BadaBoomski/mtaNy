d2c2ccd9cfb5e2a8849fb5c7847e66028f4d7afa
BadaBoomski <20119144@post.au.dk>
2018-11-22 14:40:09 +0100

    Added 4 TestCases (and 1 not functional) to ITCheckTracksAndTransponderReceiverClient.

--- UnitTestProject1/IntegrationTest/IntegrationCheckTracksAndTransponderreceiverclientAndOurairspace.cs
+++ UnitTestProject1/IntegrationTest/IntegrationCheckTracksAndTransponderreceiverclientAndOurairspace.cs
@@ -15,6 +15,7 @@ namespace UnitTestProject1
         private IOurAirspace _ourAirspace;
         private ITransponderReceiverClient _trc;
         private List<ITrack> _filteredList;
+        private int _nEventsRaised;
 
         [SetUp]
         public void Setup()
@@ -23,12 +24,44 @@ namespace UnitTestProject1
             _ourAirspace = new OurAirspace();
             _trc = new TransponderReceiverClient(_transponderReceiver);
             _uut = new CheckTracks(_ourAirspace, _trc);
+            _nEventsRaised = 0;
 
             _uut.CheckedTracks += (o, args) =>
             {
                 _filteredList = args.TrackData;
+                ++_nEventsRaised;
             };
         }
+
+        // Tried to make a TearDown to reset the _nEventsRaised after each test, but that didn't fix the problem. Problem possibly found in TransponderReceiverClient.cs?
+        [TearDown]
+        public void Teardown()
+        {
+            _nEventsRaised = 0;
+        }
+
+
+        /// <summary>
+        /// DOESN'T WORK!!! Keeps raising 3 events, when it should only raise 2. 
+        /// </summary>
+        /// <param name="approvedTrack"></param>
+        /// <param name="unapprovedTrack"></param>
+        //[TestCase("HOJ1234;20000;20000;10000;20000101235959999", "DØV542;1;20000;10000;20000101235959999")]
+        ////[TestCase("HEJ1234;20000;89999;19999;20000101235959999", "DAV542;1;1;1;20000101235959999")]
+        //public void CheckTracks_TracksAddedTwice_NumberOfEventsReceivedIsCorrect(string transponderDataInside, string transponderDataOutside)
+        //{
+        //    _nEventsRaised = 0;
+
+        //    List<string> transponderStrings = new List<string>();
+        //    transponderStrings.Add(transponderDataInside);
+        //    var args = new RawTransponderDataEventArgs(transponderStrings);
+
+        //    _transponderReceiver.TransponderDataReady += Raise.EventWith(args);
+        //    args.TransponderData.Add(transponderDataOutside);
+        //    _transponderReceiver.TransponderDataReady += Raise.EventWith(args);
+
+        //    Assert.That(_nEventsRaised, Is.EqualTo(2));
+        //}
 
         [TestCase("HEJ1234;20000;20000;10000;20000101235959999", "DAV542;1;20000;10000;20000101235959999")]
         [TestCase("HEJ1234;20000;89999;19999;20000101235959999", "DAV542;1;1;1;20000101235959999")]
@@ -52,5 +85,38 @@ namespace UnitTestProject1
             Assert.That(_filteredList[0].Tag, Is.EqualTo(approvedTracksTag));
         }
 
+        [TestCase("DAV542;1;1;1;20000101235959999")] // All below boundaries
+        [TestCase("DAV542;90001;90001;20001;20000101235959999")] // All above boundaries
+        public void ParsingToFiltering_TransponderdataStringOutsideAirspace_FilteredTracksDoesNotContainsNewTrack(string transponderDataStringOutsideOurAirspace)
+        {
+            List<string> transponderStrings = new List<string>();
+            transponderStrings.Add(transponderDataStringOutsideOurAirspace);
+            var args = new RawTransponderDataEventArgs(transponderStrings);
+
+            _transponderReceiver.TransponderDataReady += Raise.EventWith(args);
+            _transponderReceiver.TransponderDataReady += Raise.EventWith(args);
+            _transponderReceiver.TransponderDataReady += Raise.EventWith(args);
+            _transponderReceiver.TransponderDataReady += Raise.EventWith(args);
+
+
+            Assert.That(_filteredList.Count, Is.EqualTo(0));
+        }
+
+        [TestCase("DAV542;10000;10000;500;20000101235959999")] // All exactly equal to lower boundaries
+        [TestCase("DAV542;90000;90000;20000;20000101235959999")] // All exactly equal to upper boundaries
+        public void ParsingToFiltering_TransponderdataStringOutsideAirspace_FilteredTracksDoesContainsNewTrack(string transponderDataStringOutsideOurAirspace)
+        {
+            List<string> transponderStrings = new List<string>();
+            transponderStrings.Add(transponderDataStringOutsideOurAirspace);
+            var args = new RawTransponderDataEventArgs(transponderStrings);
+
+            _transponderReceiver.TransponderDataReady += Raise.EventWith(args);
+            _transponderReceiver.TransponderDataReady += Raise.EventWith(args);
+            _transponderReceiver.TransponderDataReady += Raise.EventWith(args);
+            _transponderReceiver.TransponderDataReady += Raise.EventWith(args);
+
+
+            Assert.That(_filteredList.Count, Is.EqualTo(1));
+        }
     }
 }
