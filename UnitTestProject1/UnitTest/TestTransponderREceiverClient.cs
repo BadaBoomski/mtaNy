93ba58ca24cf06d917794db451675244a5a45241
BadaBoomski <20119144@post.au.dk>
2018-11-22 12:26:20 +0100

    minor Github fuckups...

---
+++ UnitTestProject1/UnitTest/TestTransponderREceiverClient.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleApp3;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ConsoleApp3.Domain;
+using TransponderReceiver;
+using NSubstitute;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    class TransponderReceiverClientTest
+    {
+
+        private List<ITrack> _trackList;
+        private int _NrEvents;
+        private TransponderReceiverClient _uut;
+        private ITransponderReceiver _receiver { get; set; }
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _uut = new TransponderReceiverClient(_receiver);
+
+            _uut.ReadyTracks += (o, args) =>
+            {
+                _trackList = args.TrackData;
+                _NrEvents++;
+            };
+        }
+
+
+        [TestCase("TAG01;23310;44251;11368;19999101136959128")]
+        public void CreateTrackAndCheckIfExistsInList(string track)
+        {
+
+            var transpondedData = new List<string>();
+            transpondedData.Add(track);
+            var arg = new RawTransponderDataEventArgs(transpondedData);
+
+            _receiver.TransponderDataReady += Raise.EventWith(arg);
+
+            Assert.That(_trackList[0].Tag, Is.EqualTo(track.Split(';') [0]));
+            Assert.That(_trackList[0].XCoordinate, Is.EqualTo(Int32.Parse(track.Split(';')[1])));
+            Assert.That(_trackList[0].YCoordinate, Is.EqualTo(Int32.Parse(track.Split(';')[2])));
+            Assert.That(_trackList[0].Altitude, Is.EqualTo(Int32.Parse(track.Split(';')[3])));
+            Assert.That(_trackList[0].Timestamp, Is.EqualTo(DateTime.ParseExact(track.Split(';')[4], "yyyyMMddHHmmssfff", null)));
+            Assert.That(_NrEvents, Is.EqualTo(1));
+        }
+}
+}
